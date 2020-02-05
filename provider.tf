0d8e846825e206dfd1456ea443060d6ef5187596
Kunal Malhotra <kunalmalhotra@Kunals-MacBook-Pro.local>
2020-02-05 11:41:26 +0400

    Bug Fix

--- provider.tf
+++ provider.tf
@@ -7,9 +7,9 @@ provider "ibm" {
   // 
   // When using Schematics you do not need an API Key - it is retrieved for you
   // You only need a classic_username when creating classic IaaS/PaaS resources
-  //iaas_classic_username = "${var.iaas_classic_username}"
-  //iaas_classic_api_key  = "${var.iaas_classic_api_key}"
-  //ibmcloud_api_key      = "${var.ibmcloud_api_key}"
+  iaas_classic_username = "${var.iaas_classic_username}"
+  iaas_classic_api_key  = "${var.iaas_classic_api_key}"
+  ibmcloud_api_key      = "${var.ibmcloud_api_key}"
   generation            = 2
   region                = "${var.ibmcloud_region}"
 }
